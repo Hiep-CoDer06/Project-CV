3a196570c8c1cc045bb2359e8d2458dc63dfe833
Đàm Đình Hiệp <damhiep98ns@gmail.com>
2025-09-29 22:57:35 +0700

    First commit - CV Website

---
+++ javascrip.js
@@ -0,0 +1,24 @@
+const sections = document.querySelectorAll(
+  "section, .section-one, .section-two, .section-three, .section-four"
+);
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 100;
+    const sectionHeight = section.clientHeight;
+
+    if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
